bcc11fc1a7b81e5ade0d647f50cafe0fdb5388a4
HamzaNaser05 <hamzah.m.nasser@gmail.com>
2026-06-19 23:55:29 +0300

    1- Auth Controllers Added

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({extended: true})); // to parse urlencoded data
+
+console.log(process.env.MONGO_URI);
+app.use(cookieParser())
+app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+});
